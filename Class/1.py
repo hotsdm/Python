bcfe9160e39fd0fe558644815be5a401cbe6583c
hotsdm <harmoniax@gmail.com>
2019-01-13 20:11:35 +0900

    190113 -  메소드 subtract의 오타 수정

--- Class/1.py
+++ Class/1.py
@@ -1,4 +1,4 @@
-class Cal(object):
+class Cal(object): # object는 써도 되고 안 써도 됨. 상속과 관련된 것
     def __init__(self, v1, v2): # 첫 번째 매개 변수(self, 이름 바꿔도 됨)는 꼭 지정해야 함! 인스턴스 변수 지정 시 앞에 붙음
         self.v1 = v1
         self.v2 = v2
@@ -7,7 +7,7 @@ class Cal(object):
         return self.v1 + self.v2
         
     def subtract(self):
-        return self.v1 + self.v2
+        return self.v1 - self.v2
 
 c1 = Cal(10,10)
 print(c1.add())
