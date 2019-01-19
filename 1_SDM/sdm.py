9a134e74c48c8d7052f8543c8b738a530198aa6e
hotsdm <harmoniax@gmail.com>
2019-01-19 17:36:44 +0900

    190119 - SDM 폴더 생성 및 sdm.py 일부 내용 수정

---
+++ 1_SDM/sdm.py
@@ -0,0 +1,44 @@
+class Cal_margin(object):
+        def __init__(self, tp, mg):
+                self.tp = int(tp)                
+                self.mg = 1 - float(mg)
+                self.duty_ise = 1.08
+                self.duty_ksc = 1.05                
+
+        def ksc_m(self):
+                import math                
+                ksc_tp = self.tp * self.duty_ksc
+                return math.ceil(ksc_tp / self.mg)
+
+class Cal_m_ise(Cal_margin):
+        def ise_m(self):      
+                import math 
+                ise_tp = self.tp * self.duty_ise
+                return math.ceil(ise_tp / self.mg)
+
+input_cust = input("고객명을 입력해주세요. \n(경신=ksc, 영화=yht, ISE=ise, 브라이스톤=bs) \n")
+input_tp = input("TP를 입력해주세요. \n")
+input_mg = input("마진을 입력해주세요. \n")
+
+customers = ['ksc', 'ise', 'yht', 'bs']
+for cust in customers:
+        if cust == input_cust:
+                print('True')
+                import sys
+                sys.exit()
+        print('False')
+        
+
+
+# if input_cust == members[0]:
+#         input_ksc = Cal_margin(input_tp, input_mg)
+#         print("계산된 경신의 SP는 " + str(input_ksc.ksc_m()) + "입니다.")
+# elif input_cust == 'ise':
+#         input_ise = Cal_m_ise(input_tp, input_mg)
+#         print(input_ise.ise_m())
+# elif input_cust == 'yht':
+#         print("아직 설정되지 않았습니다.")
+# elif input_cust == 'bs':
+#         print("아직 설정되지 않았습니다.")
+# else:
+#         print("고객명 입력이 잘못 되었습니다.")
